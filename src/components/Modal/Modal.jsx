6ddead599788fa0669e0f82237565e80f24097d9
Waterlilyrr <noemict94@hotmail.it>
2022-07-29 18:40:38 +0200

    feat: delete message modal styled

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,25 +1,37 @@
-import Button from '../Button';
-import './index.css';
+import Button from "../Button";
+import "./index.css";
 
-const Modal = ({modalTextContent, onModalConfirm, setModalVisibility}) => {
+const Modal = ({ modalTextContent, onModalConfirm, setModalVisibility }) => {
   const onConfirmBtn = () => {
     onModalConfirm();
     setModalVisibility(false);
-  }
+  };
 
   const onCancelBtn = () => {
     setModalVisibility(false);
-  }
+  };
 
   return (
     <div className="Modal">
-      <p>{ modalTextContent }</p>
+      <p>{modalTextContent}</p>
       <div className="Modal__btns">
-        <Button onHandleClick={onCancelBtn} btnTextContent="Annulla" color="lightcoral" type="button" />
-        <Button onHandleClick={onConfirmBtn} btnTextContent="Conferma" color="lightseagreen" type="button" />
+        <Button
+          onHandleClick={onCancelBtn}
+          className="btn__annulla"
+          btnTextContent="Annulla"
+          color="rgb(195, 42, 200)"
+          type="button"
+        />
+        <Button
+          onHandleClick={onConfirmBtn}
+          className="btn__conferma"
+          btnTextContent="Conferma"
+          color="#0fffa3"
+          type="button"
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Modal;
